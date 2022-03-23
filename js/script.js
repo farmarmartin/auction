f2710afb9fceb7ba9e2de911fafa44dea448ffcf
Martin Job <90215739+farmarmartin@users.noreply.github.com>
2022-03-23 10:50:51 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,10 +7,12 @@ for(let index in data){
     let p = document.createElement("div");
     p.innerHTML = `<img src="${item.images[0]}"><h2 id="name">${item.name}</h2><p> ${item.description}</p><p> ${item.author}</p><p class="price">${item.price}</p>`;
     gallery.appendChild(p);
-}
 
-document.getElementById("name").onclick = function() {popUp()};
-
-function popUp(){
-
+    p.onclick = () => {
+        detail.innerHTML = `<div class="background"><div class="deta"><img src="${item.images[0]}"  class="detailPic"></div></div>`;
+        detail.style.display = "flex";
+    }
+    detail.onclick = () => {
+        detail.style.display = "none"
+    }
 }
